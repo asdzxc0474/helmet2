743e1a2781067ff531252d7337acdbda21eea4f5
DWEIEEE <116296953+DWEIEEE@users.noreply.github.com>
2023-03-21 13:07:50 +0800

    Update test_base64.py

--- test_base64.py
+++ test_base64.py
@@ -3,13 +3,17 @@ import os
 import cv2
 from detect import detect
 
-base64_str = '' #base64_path
-detector = detect()
-
-while True:
-    img = base64.b64decode(base64_str)  
-    frame = detector.predict(img)
-    frame = cv2.resize(frame,(1280,720))
-    cv2.imshow('test',frame)
-    if cv2.waitKey(1)==ord('q'):
-        break
+class test_base64_func(base64_str):
+    
+    detector = detect()
+    while True:
+        img = base64.b64decode(base64_str)  
+        frame = detector.predict(img)
+        frame = cv2.resize(frame,(1280,720))
+        cv2.imshow('test',frame)
+        if cv2.waitKey(1)==ord('q'):
+            break
+            
+if __name__ == '__main__':
+    base64_str = '' #base64_path
+    test_base64_func()
