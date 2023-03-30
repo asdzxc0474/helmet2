6fdaee3882f145d8ad090f82a44f05414061369c
asdzxc0474 <90667788+asdzxc0474@users.noreply.github.com>
2023-03-30 10:45:30 +0800

    Update run.py

--- run.py
+++ run.py
@@ -9,17 +9,20 @@ import time
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
+
+
 @app.route('/', methods=['POST', 'GET'])
 def getinput():
-    # img_str = request.form['image']
-    gettxt = requests.post('http://192.168.1.114:5000/upload')
-    img_str = gettxt.text
+    t1 = time.time()
+    img_str = request.form['image']
     img_byte = base64.b64decode(img_str)
     image = np.fromstring(img_byte, np.uint8)
     images = cv2.imdecode(image, cv2.IMREAD_ANYCOLOR)
-    frame, pred = detector.predict(images)
-    result = pred
+    frame,pred = detector.predict(images)
+    t2 = time.time()
+    result = int(pred)
+
     return str(result)
 if __name__ =='__main__':
     detector = detect()
-    app.run(host='192.168.1.114', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8888, debug=True)
